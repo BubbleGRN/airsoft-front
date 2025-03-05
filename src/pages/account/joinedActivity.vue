1329d79934d2215bf3c9cf351a536e7b71a671f1
BubbleGRN <henry03169@gmail.com>
2025-03-05 08:30:08 +0800

    3/5 final change #1

--- src/pages/account/joinedActivity.vue
+++ src/pages/account/joinedActivity.vue
@@ -1,12 +1,17 @@
 <template>
   <v-container>
+    <v-row style="position: relative; top: 80px">
+      <v-col cols="12">
+        <h1 class="text-center">{{ $t('nav.rent') }}</h1>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <route lang="yaml">
-  meta:
-    layout: account
-    login: true
-    admin: false
-    title: 'nav.joinedActivity'
-  </route>
+meta:
+  layout: account
+  login: true
+  admin: false
+  title: 'nav.joinedActivity'
+</route>
